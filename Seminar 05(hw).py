e5fae842ce6795d08fe658756760a136f54ba689
Sergey_Tyutvin <tyutvi@gmail.com>
2022-09-19 21:12:16 +0300

    Задача №3 решена

--- Seminar 05(hw).py
+++ Seminar 05(hw).py
@@ -54,45 +54,106 @@
 # Я не хочу писать код про конфеты. Кроме того, 2021 конфета! Вы представляете, сколько ходов должны сделать игроки?
 # Они быстрее умрут от старости, чем закончат игру. Код будет про игру в монетки. Условие игры в теле кода.
  
-from random import randint
+# from random import randint
 
-def input_dat(name):
-    x = int(input(f"{name}, введите количество монет, которое возьмете (от 1 до 3): "))
-    while x < 1 or x > 3:
-        x = int(input(f"{name}, введите корректное количество монет: "))
-    return x
+# def input_dat(name):
+#     x = int(input(f"{name}, введите количество монет, которое возьмете (от 1 до 3): "))
+#     while x < 1 or x > 3:
+#         x = int(input(f"{name}, введите корректное количество монет: "))
+#     return x
 
-def p_print(name, k, counter, value):
-    print(f"Походил {name}, он взял {k} монеты(у), теперь у него {counter} монет(ы). Осталось на столе {value} монет(ы).")
+# def p_print(name, k, counter, value):
+#     print(f"Походил {name}, он взял {k} монеты(у), теперь у него {counter} монет(ы). Осталось на столе {value} монет(ы).")
 
-print("На столе 11 монет. Каждый игрок берет от 1 до 3 монет. Проигрывает взявший последнюю монету.")
-player1 = input("Введите имя первого игрока: ")
-player2 = input("Введите имя второго игрока: ")
-value = 11 # стартовое количество монет
-flag = randint(0,2) # флаг очередности
-if flag:
-    print(f"Первый ходит {player1}")
-else:
-    print(f"Первый ходит {player2}")
+# print("На столе 11 монет. Каждый игрок берет от 1 до 3 монет. Проигрывает взявший последнюю монету.")
+# player1 = input("Введите имя первого игрока: ")
+# player2 = input("Введите имя второго игрока: ")
+# value = 11 # стартовое количество монет
+# flag = randint(0,2) # флаг очередности
+# if flag:
+#     print(f"Первый ходит {player1}")
+# else:
+#     print(f"Первый ходит {player2}")
 
-counter1 = 0 
-counter2 = 0
+# counter1 = 0 
+# counter2 = 0
 
-while value > 1:
-    if flag:
-        k = input_dat(player1)
-        counter1 += k
-        value -= k
-        flag = False
-        p_print(player1, k, counter1, value)
-    else:
-        k = input_dat(player2)
-        counter2 += k
-        value -= k
-        flag = True
-        p_print(player2, k, counter2, value)
+# while value > 1:
+#     if flag:
+#         k = input_dat(player1)
+#         counter1 += k
+#         value -= k
+#         flag = False
+#         p_print(player1, k, counter1, value)
+#     else:
+#         k = input_dat(player2)
+#         counter2 += k
+#         value -= k
+#         flag = True
+#         p_print(player2, k, counter2, value)
 
-if flag:
-    print(f"Выиграл {player2}")
-else:
-    print(f"Выиграл {player1}")
+# if flag:
+#     print(f"Выиграл {player2}")
+# else:
+#     print(f"Выиграл {player1}")
+#---------------------------------------------
+
+# Задача №3. Создайте программу для игры в "Крестики-нолики".
+
+# Интересно, а это точно пригодится аналитику?..
+
+board = list(range(1,10))
+
+def draw_board(board):
+    print ("-" * 13)
+    for i in range(3):
+        print ("|", board[0+i*3], "|", board[1+i*3], "|", board[2+i*3], "|")
+        print ("-" * 13)
+
+def take_input(player_token):
+    valid = False
+    while not valid:
+        player_answer = input("Куда поставим " + player_token+"? ")
+        try:
+            player_answer = int(player_answer)
+        except:
+            print ("Некорректный ввод. Вы уверены, что ввели число?")
+            continue
+        if player_answer >= 1 and player_answer <= 9:
+            if (str(board[player_answer-1]) not in "XO"):
+                board[player_answer-1] = player_token
+                valid = True
+            else:
+                print ("Эта клеточка уже занята")
+        else:
+            print ("Некорректный ввод. Введите число от 1 до 9, чтобы походить.")
+
+def check_win(board):
+    win_coord = ((0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6))
+    for each in win_coord:
+        if board[each[0]] == board[each[1]] == board[each[2]]:
+            return board[each[0]]
+    return False
+
+def main(board):
+    counter = 0
+    win = False
+    while not win:
+        draw_board(board)
+        if counter % 2 == 0:
+            take_input("X")
+        else:
+            take_input("O")
+        counter += 1
+        if counter > 4:
+            tmp = check_win(board)
+            if tmp:
+                print (tmp, "выиграл!")
+                win = True
+                break
+        if counter == 9:
+            print ("Ничья!")
+            break
+    draw_board(board)
+
+main(board)
